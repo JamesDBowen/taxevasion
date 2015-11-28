135b1dde7609937661318fc8ab918ae62fdb3ab1
Laereom <JamesDBowen@gmail.com>
2015-11-28 14:36:06 -0800

    Actual project
    Actual Project

---
+++ Assets/Scripts/FungusCallScript.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class FungusCallScript : MonoBehaviour {
+	// Use this for initialization
+	public GameObject UIGrid;
+	void Start () {
+	
+	}
+
+	public void FungusTestCall()
+	{
+		Debug.Log ("Method successfully called from Fungus.");
+		UIGrid.SetActive (true);
+	}
+
+	public void ReturnToFungus()
+	{
+		UIGrid.SetActive (false);
+		Fungus.Flowchart.BroadcastFungusMessage ("TestBLOCK2");
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
