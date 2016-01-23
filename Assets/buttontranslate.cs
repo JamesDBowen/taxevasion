9f9de235e17e14f67c658419660255f77989b22c
ineatprophet <mopping.accident@gmail.com>
2016-01-23 12:23:15 -0800

    BUTTON TRANSLATE
    added a sample button for translation

---
+++ Assets/buttontranslate.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class buttontranslate : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	public void ButtonTranslate()
+	{
+		Vector3 destination = this.gameObject.transform.position;
+		destination.x = destination.x + 1;
+		this.gameObject.transform.Translate (destination);
+	}
+}
