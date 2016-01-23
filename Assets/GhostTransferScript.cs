b410a0ba31c96d56214888526ecb3885b630d3b4
ineatprophet <mopping.accident@gmail.com>
2016-01-23 15:58:00 -0800

    Started Ghost Block Stuffs

---
+++ Assets/GhostTransferScript.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GhostTransferScript : MonoBehaviour {
+
+	public GameObject ghost;
+
+	void Start()
+	{
+		Vector3 originLocation = this.gameObject.transform.position;
+	}
+
+	public void TransferAnimation(bool ghostTarget)
+	{
+		Vector3 startLocation;
+		Vector3 endLocation;
+
+	}
+}
