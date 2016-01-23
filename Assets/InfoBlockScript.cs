dda0d547d7f36cc5df9b80748d904ac08760d7ce
ineatprophet <mopping.accident@gmail.com>
2016-01-23 14:17:16 -0800

    Infoblock Object Creation

---
+++ Assets/InfoBlockScript.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class InfoBlockScript : MonoBehaviour {
+
+	public string blockTitle; //quick reference to posessed knowledge
+	public bool visibleTitle; //can the player see it?
+	public string mouseText; //displays to user more in-depth detail 
+	public string blockOwner;  // Us/Them
+	public int blockID; //easy referencing
+
+}
