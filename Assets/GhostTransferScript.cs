cdd88f011748773d2dd69b571afd8f5cbc626428
ineatprophet <mopping.accident@gmail.com>
2016-01-30 12:51:22 -0800

    Transfer roughing
    Still buggy, but the ghost is moving

--- Assets/GhostTransferScript.cs
+++ Assets/GhostTransferScript.cs
@@ -4,16 +4,58 @@ using System.Collections;
 public class GhostTransferScript : MonoBehaviour {
 
 	public GameObject ghost;
+	public GameObject gwen;
+	public GameObject otherChar;
+	public GameObject targetLocation;
 
-	void Start()
-	{
-		Vector3 originLocation = this.gameObject.transform.position;
+	public float moveSpeed = 10f;
+	public bool isMoving = false;
+
+	void Start(){
+		StartAnimation (otherChar, gwen);
+		Debug.Log (otherChar.transform.localPosition + "   " + gwen.transform.localPosition);
+		Debug.Log (ghost.transform.localPosition);
 	}
 
-	public void TransferAnimation(bool ghostTarget)
-	{
-		Vector3 startLocation;
-		Vector3 endLocation;
+	void Update(){
+		if (isMoving == true) {
+			TransferAnimation(targetLocation);
+		}
+	}
+
+	public void StartAnimation(GameObject startPoint, GameObject endPoint){
+		Debug.Log ("StartAnimation Called.");
+		ghost.transform.position = startPoint.transform.position;
+		targetLocation = endPoint; //set destination
+		isMoving = true;
+		ghost.transform.LookAt (targetLocation.transform.position); //face the target destination
+
+	}
+
+	public void TransferAnimation(GameObject ghostTarget){
+
+		if (ghostTarget == otherChar) {				//when we're moving towards the other character
+			if (ghost.transform.localPosition.x >= ghostTarget.transform.localPosition.x) { //check to see if we should stop
+				isMoving = false;
+				Debug.Log ("isMoving set false.");
+			} 
+			else {       //if we don't need to stop, keep going.
+				//ghost.transform.LookAt (ghostTarget.transform.position); //face the target destination
+				ghost.transform.Translate (ghost.transform.forward * moveSpeed * Time.deltaTime);	
+			}
+		}
+		else {		//we're going towards Gwen
+			if (ghost.transform.localPosition.x <= ghostTarget.transform.localPosition.x) { //check to see if we should stop
+				isMoving = false;
+				Debug.Log("isMoving set false.");
+			} 
+			else {       //if we don't need to stop, keep going.
+				//ghost.transform.LookAt(ghostTarget.transform.position); //face the target destination
+				ghost.transform.Translate (ghost.transform.forward * moveSpeed * Time.deltaTime);	
+			}
+		}
+
+
 
 	}
 }
