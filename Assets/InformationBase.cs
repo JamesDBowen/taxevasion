dc14ecd1c72bb1d139bc278d8b98c29269487082
Laereom <JamesDBowen@gmail.com>
2016-02-13 13:37:18 -0800

    Added Information Management System

---
+++ Assets/InformationBase.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class InformationBase : ScriptableObject {
+	string title;
+	string description;
+	string mouseOver;
+
+	void Start(){
+	}
+	// Use this for initialization
+	public InformationBase (string title, string description, string mouseOver) {
+		this.title = title;
+		this.description = description;
+		this.mouseOver = mouseOver;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
--- Assets/InformationSystem.cs
+++ Assets/InformationSystem.cs
@@ -2,32 +2,49 @@
 using System.Collections;
 using System.IO;
 using System.Collections.Generic;
+using Fungus;
 
-public static class InformationSystem : MonoBehaviour {
+public class InformationSystem : MonoBehaviour {
 
-	ArrayList 
+
 	// Use this for initialization
-	void Load() {
-		var reader = new StreamReader(File.OpenRead(@"C:\test.csv"));
-
+	void Start () {
+		var reader = new StreamReader(File.OpenRead(@"C:\test.dat"));
+		
+		Dictionary<string, List<InformationBase>> info_dictionary =
+			new Dictionary<string, List<InformationBase>>();
+		
 		while (!reader.EndOfStream)
 		{
-			string character_line = reader.ReadLine().Split("|");
+			string[] character_line = reader.ReadLine().Split('|');
 			string character_name = character_line[0];
 			string characters_blocks = character_line[1];
-			List<string> blocks = new List<string>();
-			foreach (string block in characters_blocks.Split(";")){
-				blocks.Add(block);
-				List<string> block_data = new List<string>();
-				foreach (string block in blocks){
-					block_data.Add(block.Split(','));
-				}
+			List<InformationBase> character_information = new List<InformationBase>();
+			foreach (string information_data in characters_blocks.Split(';')){
+				string[] block_data = information_data.Split (',');
+				InformationBase info = new InformationBase(block_data[0],block_data[1],block_data[2]);
+				character_information.Add(info);
+			}
+			info_dictionary.Add(character_name,character_information);
+		}
+
+		foreach(GameObject character_game_object in GameObject.FindGameObjectsWithTag("Character")){
+			character_game_object.AddComponent<CharacterData>();
+			CharacterData character = character_game_object.GetComponent<CharacterData>();
+			string character_name = character_game_object.GetComponent<Character>().name;
+			if (info_dictionary.ContainsKey(character_name))
+			{
+				character.infoOwned = info_dictionary[character_name];
 			}
 		}
-		ArrayList characters;
-		characters = GameObject.FindGameObjectsWithTag("Character");
-		foreach (GameObject character in characters){
-			character.infoOwned = 
-		}
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+	// Use this for initialization
+	void Load() {
 	}
 }
