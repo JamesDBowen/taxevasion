06956551a4deee0b9476a3b4d5e89c60957dcc82
ineatprophet <mopping.accident@gmail.com>
2016-04-16 16:28:19 -0700

    Layout

--- Assets/InterviewScript.cs
+++ Assets/InterviewScript.cs
@@ -5,45 +5,41 @@ using System.Collections.Generic;
 public class InterviewScript : MonoBehaviour {
 
 	public GameObject lindsaybuckets;
+	public GameObject gwen;
+	public GameObject infoblockprefab;
+	public GameObject choicePane;
+
 	public void Start()
 	{
 		InitializeInterview (lindsaybuckets);
 
 	}
 
-	public GameObject gwen;
-	public GameObject infoblockprefab;
-	public GameObject choicePane;
+
 
 	void InitializeInterview(GameObject interviewee){
-		Debug.Log ("Interview Initialized. Interviewee is " + interviewee);
-		this.gameObject.SetActive (true);
-		GameObject[] characters = GameObject.FindGameObjectsWithTag ("Character");
+		//Debug.Log ("Interview Initialized. Interviewee is " + interviewee);
+		//GameObject[] characters = GameObject.FindGameObjectsWithTag ("Character");
 		List<InformationBase> gwenIntel = gwen.GetComponent<CharacterData> ().infoOwned;
 		List<InformationBase> otherIntel = interviewee.GetComponent<CharacterData> ().infoOwned;
-
+		//Debug.Log (otherIntel.Count);
 		foreach (InformationBase intel in gwenIntel)
 		{
 			//instantiate an infoblock
 			GameObject newBlock = (GameObject)Instantiate(infoblockprefab);
 			newBlock.transform.SetParent(GameObject.Find("Gwens Infoblock Cache").transform);
+			newBlock.GetComponent<RectTransform>().localScale = new Vector3(1,1,1);
 
 		}
+		Debug.Log ("Done with Gwen. Now the next one.");
 		foreach (InformationBase intel in otherIntel)
 		{
+			//Debug.Log (otherIntel.Count);
 			//instantiate an infoblock
 			GameObject newBlock = (GameObject)Instantiate(infoblockprefab);
-			newBlock.transform.parent = GameObject.Find("NPCs Infoblock Cache").transform;
+			newBlock.transform.SetParent(GameObject.Find("NPCs InfoBlock Cache").transform);
+			newBlock.GetComponent<RectTransform>().localScale = new Vector3(1,1,1);
+			Debug.Log ("Success");
 		}
-	}
-
-	public void Tap(GameObject block){
-		//adds infoblock to the center pane, along with choice items 
-		GameObject newPane = (GameObject)Instantiate (choicePane);
-		newPane.transform.SetParent(GameObject.Find("Dynamic Block Selection").transform);
-		}
-
-	public void Untap(InfoBlock block){
-		// if a currently tapped infoblock is clicked, untap it, kill the panel.
 	}
 }
